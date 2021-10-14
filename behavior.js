e8651b0e5bd914a4507a89b57e036f46d483a59e
luisangel <root.labm@gmail.com>
2021-10-13 19:43:30 -0500

    firts commint in web-holberton

---
+++ behavior.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById("smart_thumbnail");
+    var thumbnailElement = document.getElementById ("smart_thumbnail");
+    thumbnailElement.addEventListener("click", function() {
+     
+    });
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+   
+    thumbnailElement.className;        
+    thumbnailElement.className = "small";
+
+    if (thumbnailElement.className == "") {
+        // write here the code that will execute if the image is big
+    }
+
+       
+});
+
+
+
+
+
+
